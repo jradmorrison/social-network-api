d49698ff566d87fdc7b9607862bf3b329c0e0edb
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-11-21 12:36:00 -0500

    wrote thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLowerCase(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtuals('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
